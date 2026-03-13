5dfd89f3aa201c2dedff241b5725cef8ee5ad9af
ErrorOliver <ErrorOliver6@proton.me>
2026-03-13 00:06:50 +0000

    Update login links to point to ixchats.com

--- apps/juxtaposition-ui/src/services/juxt-web/views/web/loginView.tsx
+++ apps/juxtaposition-ui/src/services/juxt-web/views/web/loginView.tsx
@@ -18,7 +18,7 @@ export function WebLoginView(props: LoginViewProps): ReactNode {
 			</div>
 			<div className="wrapper">
 				<div className="account-form-wrapper">
-					<a className="logotype" href="https://pretendo.network">
+					<a className="logotype" href="https://ixchats.com">
 						<PretendoLogo />
 					</a>
 					<form action="/login" method="post" className="account">
@@ -31,12 +31,12 @@ export function WebLoginView(props: LoginViewProps): ReactNode {
 						<div>
 							<label htmlFor="password"><T k="login.password" /></label>
 							<input name="password" id="password" type="password" required />
-							<a href="https://pretendo.network/account/forgot-password" className="pwdreset"><T k="login.forgot_password" /></a>
+							<a href="https://ixchats.com/account/forgot-password" className="pwdreset"><T k="login.forgot_password" /></a>
 						</div>
 						<input name="grant_type" id="grant_type" type="hidden" value="password" />
 						<div className="buttons">
 							<button type="submit"><T k="login.login_action" /></button>
-							<a href="https://pretendo.network/account/register" className="register"><T k="login.no_account" /></a>
+							<a href="https://ixchats.com/account/register" className="register"><T k="login.no_account" /></a>
 						</div>
 						<input type="hidden" name="redirect" value={props.redirect ?? '/'} />
 					</form>
